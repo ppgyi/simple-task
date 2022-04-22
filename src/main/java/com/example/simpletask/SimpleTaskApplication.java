071320cf7e37403749e54cab6d47237bd4e10d74
Peter <peter.palagyi@gmail.com>
2022-04-22 08:46:29 +0200

    initial commit

---
+++ src/main/java/com/example/simpletask/SimpleTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpletask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simpletask/SimpleTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpletask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
